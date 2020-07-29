4585cbdf5f41e9462f82190c558f7a3e31b70945
sahinerkus <sahinerkus@users.noreply.github.com>
2020-07-28 22:45:02 -0500

    Final Changes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
-// const form = document.getElementById('passwordGeneratorForm')
+// Those are the vaiables for html-js connection
 const passwordDisplay = document.getElementById('password')
 const buttonEl = document.getElementById("generate")
+//Those variables are standing for character codes. Each type of character is an array of numbers. Those arrays are the limits of my random number creation. .concat helps to bumble the arrays.
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
@@ -12,34 +13,31 @@ const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
 ).concat(
   arrayFromLowToHigh(123, 126)
 )
-// characterAmountNumber.addEventListener('click', syncCharacterAmount)
 
 
+//The event listener is where we start to create our password. After we click the button, we will see the prompts and confirms to shape our password.
 buttonEl.addEventListener('click', e => {
-  console.log("inside event listener")
   e.preventDefault()
   const characterAmount = +prompt("Enter a number between 8 to 128: ")
-// const characterAmountNumber = document.getElementById('characterAmountNumber')
   const includeUppercase = confirm("Do you want upper case characters in your password?")
   const includeLowercase = confirm("Do you want lower case characters in your password?")
   const includeNumbers = confirm("Do you want numbers case in your password?")
   const includeSymbols = confirm("Do you want symbols in your password?")
-  // const includeUppercase = includeUppercaseElement
-  // const includeNumbers = includeNumbersElement
-  // const includeSymbols = includeSymbolsElement
+  //passwordDisplay addresses the password to the html loc. To do that, there is an array of character types turned in to a value.
   const password = generatePassword(characterAmount, includeUppercase, includeLowercase, includeNumbers, includeSymbols)
   passwordDisplay.value = password
 })
-
+  // The function we create the code.
 function generatePassword(characterAmount, includeUppercase, includeLowercase, includeNumbers, includeSymbols) {
-  // let charCodes = LOWERCASE_CHAR_CODES
+
   console.log("inside password generator")
   let charCodes = []
+  //where we see what we have in our password
   if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
   if (includeLowercase) charCodes = charCodes.concat(LOWERCASE_CHAR_CODES)
   if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
   if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
-  
+  //where we create random number in chosen arrays
   const passwordCharacters = []
   for (let i = 0; i < characterAmount; i++) {
     const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
@@ -47,7 +45,7 @@ function generatePassword(characterAmount, includeUppercase, includeLowercase, i
   }
   return passwordCharacters.join('')
 }
-
+//Multiple array Symbolcharcode
 function arrayFromLowToHigh(low, high) {
   const array = []
   for (let i = low; i <= high; i++) {
@@ -55,9 +53,9 @@ function arrayFromLowToHigh(low, high) {
   }
   return array
 }
-
+//Turn Character amount into a value from string
 function syncCharacterAmount(e) {
   const value = e.target.value
   characterAmountNumber.value = value
-  // characterAmountRange.value = value
+ 
 }
